3aecaedbf0b11adfbcdde8fe366fb1578f26050d
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2024-09-23 19:50:14 +0530

    [232] [Time Beats: 100.00%] [Memory Beats: 22.87%] - LeetPush

---
+++ 232-Implement-Queue-using-Stacks.cpp
@@ -0,0 +1,49 @@
+class MyQueue {
+private:
+    stack<int> input, output;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        peek();
+        int val = output.top();
+        output.pop();
+        return val;
+    }
+    
+    int peek() {
+        if(output.empty()) {
+            while(!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return output.empty() && input.empty();
+    }
+};
+const int ZERO = []()
+{
+\tstd::ios_base::sync_with_stdio(false);
+\tstd::cin.tie(nullptr);
+\treturn 0;
+}();
+
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
