8119359c14179e4a48684402383d472b8e1ebc47
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2024-09-26 16:17:47 +0530

    [503] [Time Beats: 71.90%] [Memory Beats: 77.06%] - LeetPush

---
+++ 503-Next-Greater-Element-II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for(int i=2*n-1; i>=0; i--) {
+
+            while(!st.empty() && st.top() <= nums[i%n]) {
+                st.pop();
+            }
+            if(!st.empty()) {
+                ans[i%n] = st.top();
+            }
+
+            st.push(nums[i%n]);
+        }
+
+        return ans;
+    }
+};
+const int ZERO = []()
+{
+\tstd::ios_base::sync_with_stdio(false);
+\tstd::cin.tie(nullptr);
+\treturn 0;
+}();
