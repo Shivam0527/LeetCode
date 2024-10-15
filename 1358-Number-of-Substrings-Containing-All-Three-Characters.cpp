3081a1f8a27e4544a8498af206ddb95616f7623f
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2024-10-15 18:25:15 +0530

    [1358] [Time Beats: 97.54%] [Memory Beats: 71.40%] - LeetPush

--- 1358-Number-of-Substrings-Containing-All-Three-Characters.cpp
+++ 1358-Number-of-Substrings-Containing-All-Three-Characters.cpp
@@ -1,30 +1,57 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
         
+        int n = s.size();
+
+        int lastAIndex = -1, lastBIndex = -1, lastCIndex = -1;
         int cnt = 0;
-        int left = 0, right = 0;
-        vector<int> vec(3, 0);
+        
+        for(int i=0; i<n; i++) {
+            if(s[i] == 'a') lastAIndex = i;
+            else if(s[i] == 'b') lastBIndex = i;
+            else lastCIndex = i;
 
-        while(right < n) {
-            
-            vec[s[right] - 'a']++;
-
-            while(vec[0]>0 && vec[1]>0 && vec[2]>0) {
-                cnt += (n - right);
+            if(lastAIndex != -1 && lastBIndex != -1 && lastCIndex != -1) {
+                int minIndex = min(lastAIndex, min(lastBIndex, lastCIndex));
                 
-                vec[s[left] - 'a']--;
-
-                left++;
-            }        
-
-            right++;
-        }        
+                cnt += (minIndex + 1);
+            }
+        }
 
         return cnt;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int n = s.size();
+        
+//         int cnt = 0;
+//         int left = 0, right = 0;
+//         vector<int> vec(3, 0);
+
+//         while(right < n) {
+            
+//             vec[s[right] - 'a']++;
+
+//             while(vec[0]>0 && vec[1]>0 && vec[2]>0) {
+//                 cnt += (n - right);
+                
+//                 vec[s[left] - 'a']--;
+
+//                 left++;
+//             }        
+
+//             right++;
+//         }        
+
+//         return cnt;
+//     }
+// };
 const int ZERO = []()
 {
 	std::ios_base::sync_with_stdio(false);
