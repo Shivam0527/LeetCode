ba696540d30bf6317cae10f729c40c4e2e091a2d
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2024-10-24 01:11:09 +0530

    [2641] [Time Beats: 94.18%] [Memory Beats: 25.99%] - LeetPush

---
+++ 2641-Cousins-in-Binary-Tree-II.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        
+        if(!root) return root;
+
+        queue<TreeNode*> que;
+        vector<long long> levelSum;
+
+        que.push(root);
+
+        while(!que.empty()) {
+
+            int n = que.size();
+            long long sum = 0;
+
+            while(n > 0) {
+                TreeNode* temp = que.front();
+
+                que.pop();
+                sum += temp->val;
+
+                if(temp->left) que.push(temp->left);
+                if(temp->right) que.push(temp->right);
+
+                n--;
+            }
+
+            levelSum.push_back(sum);
+        }
+
+        que.push(root);
+
+        root->val = 0;
+        int i = 1;
+
+        while(!que.empty()) {
+
+            int n = que.size();
+
+            while(n > 0) {
+                TreeNode* temp = que.front();
+                que.pop();
+
+                long long siblingSum = temp->left != NULL ? temp->left->val : 0;
+                siblingSum += temp->right != NULL ? temp->right->val : 0;
+
+                if(temp->left) {
+                    temp->left->val = levelSum[i] - siblingSum;
+                    que.push(temp->left);
+                }
+                if(temp->right) {
+                    temp->right->val = levelSum[i] - siblingSum;
+                    que.push(temp->right);
+                }
+
+                n--;
+            }
+
+            i++;
+        }
+
+        return root;        
+    }
+};
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
