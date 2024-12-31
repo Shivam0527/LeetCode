1428a750cac29b0075d89ff82536cd968d021997
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2024-12-31 23:18:11 +0530

    [412] [Time Beats: 100.00%] [Memory Beats: 26.77%] - LeetPush

---
+++ 412-Fizz-Buzz.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        if(n == 0)
+            return ans;
+
+        for(int i=1; i<=n; i++) {
+
+            if(i%3 == 0 && i%5 == 0)
+                ans.push_back(\FizzBuzz\);
+            else if(i%3 == 0)
+                ans.push_back(\Fizz\);
+            else if(i%5 == 0)
+                ans.push_back(\Buzz\);
+            else 
+                ans.push_back(to_string(i));
+        }
+
+        return ans;        
+    }
+};
+const int ZERO = []()
+{
+\tstd::ios_base::sync_with_stdio(false);
+\tstd::cin.tie(nullptr);
+\treturn 0;
+}();
