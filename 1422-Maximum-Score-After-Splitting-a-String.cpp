4d41a37f111c5aa2eea67b8c82ec528166b64fa4
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2025-01-04 22:12:31 +0530

    [1422] [Time Beats: 100.00%] [Memory Beats: 19.58%] - LeetPush

---
+++ 1422-Maximum-Score-After-Splitting-a-String.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        
+        if(nums.size() % k != 0) return false;
+
+        map<int, int> mpp;
+
+        for(int it : nums) mpp[it]++;
+
+        while(!mpp.empty()) {
+
+            int curr = mpp.begin()->first;
+
+            for(int i=0; i<k; i++) {
+
+                if(mpp[curr + i] == 0){
+                    return false;
+                }
+
+                mpp[curr + i]--;
+
+                if(mpp[curr + i] < 1) mpp.erase(curr + i);
+            }
+        }
+
+        return true;
+    }
+};
+const int ZERO = []()
+{
+\tstd::ios_base::sync_with_stdio(false);
+\tstd::cin.tie(nullptr);
+\treturn 0;
+}();
